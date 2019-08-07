43a7c0e84f285edcc7647856117cd80a242ff3cb
jaeyeon-park <parkjani2@gmail.com>
2019-08-07 05:47:04 +0000

    add shebang

--- run_bjoern_with_log.sh
+++ run_bjoern_with_log.sh
@@ -1,7 +1,6 @@
+#!/usr/bin/env bash
 virtual_env="./flask-env"
 bjoern_py=bjoern_run.py
 log_txt=./log.txt
-touch ${log_txt}
-deactivate
 source ${virtual_env}/bin/activate
-python ${bjoern_py} >> ./log.txt
+python ${bjoern_py} > ${log_txt}
