fc260aedfec1a5c1d778a8e60aa89d5deac34927
jaeyeon-park <parkjani2@gmail.com>
2019-08-25 13:55:49 +0900

    apply SSL_CERTIFICATE

--- bjoern_run.py
+++ bjoern_run.py
@@ -1,20 +1,20 @@
-import sys,os
+import sys,os,socket,ssl
 import bjoern
 current_path = os.path.abspath('./')
 sys.path.append(current_path)
 from ppaa import create_app
-
+from ppaa.config import ssl_config
 
 if __name__ == "__main__":
     default_ip = "0.0.0.0"
     default_port = 8080
     ip = default_ip
     port = default_port
+    #create app
     app = create_app(DEBUG=False)
     print("----------------new start----------------")
     print("server start on {}".format(current_path))
     bjoern.run(app,ip,port)
-
 
     
 
