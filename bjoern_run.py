a8a3becc1f1a67e840f3e15aeeb2becbdd6ba2c6
jaeyeon-park <parkjani2@gamil.com>
2019-08-25 06:05:16 +0000

    rm ssl_config

--- bjoern_run.py
+++ bjoern_run.py
@@ -3,7 +3,6 @@ import bjoern
 current_path = os.path.abspath('./')
 sys.path.append(current_path)
 from ppaa import create_app
-from ppaa.config import ssl_config
 
 if __name__ == "__main__":
     default_ip = "0.0.0.0"
